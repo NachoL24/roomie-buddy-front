6d99b0ac6fd13fee4f22e8aeeef5b1bec8d3789e
NachoL24 <ignaciolances24@gmail.com>
2025-07-22 21:05:47 -0300

    feat: integrate Auth0 for authentication and update environment configuration
    - Added Auth0 authentication support in app.config.ts and app.ts.
    - Updated app.html to include a login button for Auth0.
    - Created an example environment configuration file (env.example.ts) for API and Auth0 settings.
    - Updated .gitignore to exclude environment variable files.
    - Enhanced index.html with Google Fonts and Material Icons.
    - Added global styles and theming for Angular Material in styles.scss.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from '../environments/env';
+
+export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAuth0({
+      domain: environment.AUTH0_DOMAIN,
+      clientId: environment.AUTH0_CLIENT_ID,
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+    { provide: API_BASE_URL, useValue: environment.API_BASE_URL }
   ]
 };
---
+++ src/environments/env.example.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  API_BASE_URL: "ejemplo.com/api",
+  AUTH0_DOMAIN: "ejemplo.auth0.com",
+  AUTH0_CLIENT_ID: "ejemplo_client_id",
+};
