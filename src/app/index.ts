de9b30a2fbb993c38b5d83384fd8eb55f26c9aee
NachoL24 <ignaciolances24@gmail.com>
2025-07-27 21:04:49 -0300

    barrel general

---
+++ src/app/index.ts
@@ -0,0 +1,14 @@
+// Domain Layer Exports
+export * from './domain/entities';
+export * from './domain/repositories';
+
+// Application Layer Exports
+export * from './application/use-cases';
+
+// Data Layer Exports
+export * from './infrastructure/dtos';
+export * from './infrastructure/mappers';
+export * from './infrastructure/rest-clients';
+
+// Infrastructure Layer Exports
+export * from './infrastructure/providers/repository.providers';
