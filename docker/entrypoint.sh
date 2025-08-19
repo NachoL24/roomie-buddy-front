e6f104e12b1dfcd2ff542f066b214773b822d5c6
NachoL24 <ignaciolances24@gmail.com>
2025-08-19 20:14:30 -0300

    changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,10 +8,10 @@ PUBLIC_DIR="/usr/share/nginx/html"
 cat > ${PUBLIC_DIR}/env.js <<EOF
 (function(w){
   w.__env = w.__env || {};
-  w.__env.API_BASE_URL = "${API_BASE_URL:-http://localhost:3000}";
-  w.__env.AUTH0_DOMAIN = "${AUTH0_DOMAIN:-dev-5vezrqyf1x1t184t.us.auth0.com}";
-  w.__env.AUTH0_CLIENT_ID = "${AUTH0_CLIENT_ID:-hyCtD4sF3LWakI8X6yWfojqapjjTVZcA}";
-  w.__env.AUTH0_AUDIENCE = "${AUTH0_AUDIENCE:-https://dev-5vezrqyf1x1t184t.us.auth0.com/api/v2/}";
+  w.__env.API_BASE_URL = "${API_BASE_URL:-pruebas}";
+  w.__env.AUTH0_DOMAIN = "${AUTH0_DOMAIN:-pruebas}";
+  w.__env.AUTH0_CLIENT_ID = "${AUTH0_CLIENT_ID:-pruebas}";
+  w.__env.AUTH0_AUDIENCE = "${AUTH0_AUDIENCE:-pruebas}";
 })(window);
 EOF
 
